8e6b023b92e79d90b8036f42e0a92677212f17f4
Nguyễn Minh Chiến <chien9pm@gmail.com>
2023-09-17 15:52:46 +0700

    demo report 3

--- src/server.js
+++ src/server.js
@@ -28,3 +28,5 @@ require('../routes/user.routes')(app);
 app.listen(port, () => {
   console.log(`Server is running on port ${port}.`);
 });
+
+module.exports = app;
--- test/user.test.js
+++ test/user.test.js
@@ -1,8 +1,19 @@
-process.env.NODE_ENV = 'test';
-
 let chai = require('chai');
 let chaiHttp = require('chai-http');
 let server = require('../src/server');
+const { authJwt, validateData } = require("../middlewares");
+const controller = require("../controllers/user.controller");
+
+server.use(function (req, res, next) {
+    res.header(
+        "Access-Control-Allow-Headers",
+        "x-access-token, Origin, Content-Type, Accept"
+    );
+    next();
+});
+
+process.env.NODE_ENV = 'test';
+
 let should = chai.should();
 
 chai.use(chaiHttp);
@@ -18,7 +29,12 @@ describe('Users', () => {
     describe('/GET users', () => {
         it('it should GET all the directory product', (done) => {
             chai.request(server)
-                .get('/api/moderator/directory/product')
+                .get(
+                    "/api/moderator/directory/product",
+                    [authJwt.verifyToken, authJwt.isModerator],
+                    controller.ModeratorDirectoryProduct
+                )
+                .set({ "x-access-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaWF0IjoxNjk0OTQwMzQ1LCJleHAiOjE2OTUwMjY3NDV9.H2sKJSNdHIVPo-_0VaSOInj-n-m9MFbUqdeE1PVhnZo" })
                 .end((err, res) => {
                     res.should.have.status(200);
                     res.body.should.be.a('array');
